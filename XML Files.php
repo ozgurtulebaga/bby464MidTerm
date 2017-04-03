c3c52682d912234c719d0a0f2e9484cadd8528e5
ozgurtulebaga <ozgur2037@hotmail.com>
2017-04-04 00:15:05 +0300

    Add files via upload

---
+++ XML Files.php
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+  <Yayincilar>
+    <Yayinci>
+      <YayinciID>21</YayinciID>
+      <Adi>Profil Kitap</Adi>
+      <Yeri>İstanbul</Yeri>
+    </Yayinci>
+    <Yayinci>
+       <YayinciID>22</YayinciID>
+       <Adi>Remzi Kitapevi</Adi>
+       <Yeri>İstanbul</Yeri>
+    </Yayinci>
+    <Yayinci>
+       <YayinciID>23</YayinciID>
+       <Adi>İs Bankası Yayınları</Adi>
+       <Yeri>İstanbul</Yeri>
+     </Yayinci>
+  </Yayincilar>    
+    
