0ed71a6adad24578fa1bff3819e7e7a34d443219
ozgurtulebaga <ozgur2037@hotmail.com>
2017-04-04 00:16:11 +0300

    Add files via upload

---
+++ RDF-XML-File 1.php
@@ -0,0 +1,48 @@
+<?xml version="1.0"?>
+
+<rdf:RDF
+xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+xmlns:dc= "http://purl.org/dc/elements/1.1/">
+
+<rdf:Description rdf:about="Kitap">
+    <dc:title>Tarihin izinde</dc:title>
+    <dc:creator>İlber Ortaylı</dc:creator>
+    <dc:date>2017</dc:date>
+    <dc:publisher>Profil Kitap</dc:publisher>
+    <dc:language>Türkçe</dc:language>  
+    <dc:description>208 sayfa</dc:description>
+</rdf:Description>   
+<rdf:Description rdf:about="Kitap">
+    <dc:title>soylesiler ve konusmalar</dc:title>
+    <dc:creator>Halil İnalcık</dc:creator>
+    <dc:date>2013</dc:date>
+    <dc:publisher>Profil Kitap</dc:publisher>
+    <dc:language>Türkçe</dc:language>  
+    <dc:description>464 sayfa</dc:description>
+</rdf:Description>   
+<rdf:Description rdf:about="Kitap">
+    <dc:title>sınırlar arasında</dc:title>
+    <dc:creator>Banu Avar</dc:date>
+    <dc:publisher>Remzi Kitapevi</dc:publisher>
+    <dc:language>Türkçe</dc:language>  
+    <dc:description>400 sayfa</dc:description>
+</rdf:Description>   
+<rdf:Description rdf:about="Kitap">
+    <dc:title>kumpanya</dc:title>
+    <dc:creator>Sait Faik Abasiyanik</dc:creator>
+    <dc:date>2016</dc:date>
+    <dc:publisher>İs bankası yayınları</dc:publisher>
+    <dc:language>Türkçe</dc:language>  
+    <dc:description>148 sayfa</dc:description>
+</rdf:Description>       
+<rdf:Description rdf:about="Kitap">
+    <dc:title>iletisim donanımları</dc:title>
+    <dc:creator>Dogan cuceloglu</dc:creator>
+    <dc:date>2014</dc:date>
+    <dc:publisher>Remzi Kitapevi</dc:publisher>
+    <dc:language>Türkçe</dc:language>  
+    <dc:description>200 sayfa</dc:description>
+</rdf:Description>
+
+
+</rdf:RDF>
